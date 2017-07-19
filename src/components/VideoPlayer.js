e09fda5ac176f19d8ffa2437803b704effc1582c
marco <marco.bahn@gmail.com>
2017-07-19 21:59:35 +0200

    create videoplayer component

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class VideoPlayer extends Component {
+
+	render() {
+
+		return (
+
+			<div>
+
+
+			</div>
+
+		);
+	}
+
+}
+
+export default VideoPlayer
