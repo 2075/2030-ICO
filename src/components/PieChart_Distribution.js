2e1928c175608b01f385ecf109851fb9ed49c0a8
marco <marco.bahn@gmail.com>
2017-07-19 21:59:23 +0200

    create piechart

---
+++ src/components/PieChart_Distribution.js
@@ -0,0 +1,63 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+import { PieChart } from 'react-d3-basic')
+
+	var width = 700,
+		height = 400,
+		value = function(d) {
+			return +d.population;
+		},
+		name = function(d) {
+			return d.age;
+		},
+		chartSeries = [
+			{
+				"field": "<5",
+				"name": "less than 5"
+			},
+			{
+				"field": "5-13",
+				"name": "5 to 13"
+			},
+			{
+				"field": "14-17",
+				"name": "14 to 17"
+			},
+			{
+				"field": "18-24",
+				"name": "18 to 24"
+			},
+			{
+				"field": "25-44",
+				"name": "25 to 44"
+			},
+			{
+				"field": "45-64",
+				"name": "45 to 64"
+			}
+		];
+
+	ReactDOM.render(
+		<PieChart
+			data= {generalChartData}
+			width= {width}
+			height= {height}
+			chartSeries= {chartSeries}
+			value = {value}
+			name = {name}
+		/>
+	, document.getElementById('data_pie')
+	)
+})()
+
+
+class RenderBase extends Component {
+
+	static propTypes = {
+		width: PropTypes.number.isRequired,
+		height: PropTypes.number.isRequired,
+	}
+
+}
+
+export default RenderBase
